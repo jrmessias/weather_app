27214993604e75b2300c7afbee71fac468e3bbbd
Júnior Messias <jrmessias@gmail.com>
2022-12-14 13:30:28 -0300

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weather_app/',
+  base: 'http://weather_app.jrmessias.com.br',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
