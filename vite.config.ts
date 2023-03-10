5ea250d2df5f6c50b39f6ce7fdd66b955739886e
Júnior Messias <jrmessias@gmail.com>
2023-03-10 14:34:25 -0300

    :rocket: Deploy stuff.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weather_app/',
+  base: '/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
